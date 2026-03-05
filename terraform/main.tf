4f5a5f364d652808405bc884225bfe8582f73b8d
temitopefunmi <temitopefunmi@yahoo.com>
2026-03-05 14:52:20 +0100

    Fix: Testing Free Tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_service_plan" "app_service_plan" {
   
   # These are now required top-level arguments, not blocks
   os_type             = "Linux"
-  sku_name            = "F1" # Free Tier
+  sku_name            = "F1" 
 }
 
 resource "azurerm_linux_web_app" "web_app" {
@@ -27,10 +27,17 @@ resource "azurerm_linux_web_app" "web_app" {
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
+    # 1. AUTOMATED STARTUP COMMAND
+    # This tells Azure exactly how to launch Next.js
+    app_command_line = "npx next start -p 8080"
     application_stack {
       node_version = "22-lts"
     }
     always_on = false # Always On is not required for Free Tier, so we set it to false
+  }
+  app_settings = {
+    "PORT" = "8080"
+    "NODE_ENV" = "production"
   }
 
   lifecycle {
