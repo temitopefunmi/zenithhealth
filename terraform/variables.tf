9db54c0bb48a474081e153513498bffbf3892e00
temitopefunmi <temitopefunmi@yahoo.com>
2026-03-05 11:05:21 +0100

    Fix: Unique App Name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,7 @@ variable "resource_group_name" {
 variable "location" {
   default = "westeurope"
 }
+
+variable "app_name" {
+  default = "zht-portal-web" # Change 'mac' to your initials or a random number
+}
