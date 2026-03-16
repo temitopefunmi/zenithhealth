4dc25af26686a12775ed3d73e87696902cde3f24
temitopefunmi <temitopefunmi@yahoo.com>
2026-03-16 13:44:19 +0100

    Phase 3 begins

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type = string
-  default = "rg-Zenith-Health"
+  default = "rg-Zenith"
 }
 
 variable "mgmt_resource_group_name" {
