022541f1c78664e6318f06c1a184f4f30ecefd65
temitopefunmi <temitopefunmi@yahoo.com>
2026-03-16 22:12:55 +0100

    Phase 3 begins

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "mgmt_resource_group_name" {
 variable "location" {
   description = "The Azure region where resources will be created"
   type = string
-  default = "uksouth"
+  default = "westeurope"
 }
 
 variable "app_name" {
