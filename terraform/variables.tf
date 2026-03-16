c26fa6011a6395a829bfd998fd99d158840bffbe
temitopefunmi <temitopefunmi@yahoo.com>
2026-03-16 12:38:20 +0100

    Phase 3 begins

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,35 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type = string
+  default = "rg-Zenith-Health"
 }
 
 variable "mgmt_resource_group_name" {
   description = "The name of the management resource group for identity and secrets"
   type = string
+  default = "rg-zenith-mgmt"
 }
 
 variable "location" {
   description = "The Azure region where resources will be created"
   type = string
+  default = "westeurope"
 }
 
 variable "app_name" {
   description = "The name of the application"
   type = string
+  default = "zht-portal-web"
 }
 
 variable "github_repo" {
   type        = string
   description = "The GitHub repository in the format 'username/repo-name'"
+  default     = "temitopefunmi/zenith-health"
 }
 
 variable "storage_account_name" {
-  
+  type        = string
+  description = "The name of the storage account for Terraform state"
+  default     = "zenithtfstate1773660616"
 }
