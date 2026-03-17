c4a885d0861c109b6e585b8ca0377ee6a38bcf4d
temitopefunmi <temitopefunmi@yahoo.com>
2026-03-17 04:44:56 +0100

    Phase 3 begins

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_linux_web_app" "web_app" {
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
-    app_command_line = "node .next/standalone/server.js"
+    app_command_line = "node server.js"
 
     application_stack {
       node_version = "22-lts"
