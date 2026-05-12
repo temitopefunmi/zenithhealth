094b965adbedd1c8a0119bb514d109eab0628497
temitopefunmi <temitopefunmi@yahoo.com>
2026-05-12 10:47:28 +0100

    Use Function resource group output for deployment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "webapp_url" {
   value = "https://${azurerm_linux_web_app.web_app.default_hostname}"
 }
 
+output "scheduler_function_resource_group" {
+  value = azurerm_linux_function_app.scheduler_function.resource_group_name
+}
+
 output "scheduler_function_name" {
   value = azurerm_linux_function_app.scheduler_function.name
 }
